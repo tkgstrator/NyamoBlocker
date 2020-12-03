f73f0a876a3b338901c5bf2e9fec528fb1b137a5
Asano Kazuna <nasawke.am@gmail.com>
2020-12-04 07:04:39 +0900

    Initial Commit

---
+++ nyamoblocker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nyamoblocker
+//
+//  Created by devonly on 2020-12-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
