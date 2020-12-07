c9e4e60630fde8c2d91888ddddec617c603b2b37
Asano Kazuna <nasawke.am@gmail.com>
2020-12-07 09:31:02 +0900

    Update blockerList.json

--- blockerList/ContentBlockerRequestHandler.swift
+++ blockerList/ContentBlockerRequestHandler.swift
@@ -13,23 +13,30 @@ class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
     func loadBlockerList(context: NSExtensionContext) {
         
         #if DEBUG
-        let fileManager = FileManager.default
-        let ruleFilePath = fileManager.containerURL(forSecurityApplicationGroupIdentifier: "group.nyamoblocker.blockerList")?.appendingPathComponent("blockerList.json")
-        if fileManager.fileExists(atPath: ruleFilePath!.path) {
-            let attachment = NSItemProvider(contentsOf: ruleFilePath!)!
-            
-            let item = NSExtensionItem()
-            item.attachments = [attachment]
-            
-            context.completeRequest(returningItems: [item], completionHandler: nil)
-        } else {
-            let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
-            
-            let item = NSExtensionItem()
-            item.attachments = [attachment]
-            
-            context.completeRequest(returningItems: [item], completionHandler: nil)
-        }
+        // ローカルファイル読み込み
+        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        
+        context.completeRequest(returningItems: [item], completionHandler: nil)
+//        let fileManager = FileManager.default
+//        let ruleFilePath = fileManager.containerURL(forSecurityApplicationGroupIdentifier: "group.nyamoblocker.blockerList")?.appendingPathComponent("blockerList.json")
+//        if fileManager.fileExists(atPath: ruleFilePath!.path) {
+//            let attachment = NSItemProvider(contentsOf: ruleFilePath!)!
+//
+//            let item = NSExtensionItem()
+//            item.attachments = [attachment]
+//
+//            context.completeRequest(returningItems: [item], completionHandler: nil)
+//        } else {
+//            let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+//
+//            let item = NSExtensionItem()
+//            item.attachments = [attachment]
+//
+//            context.completeRequest(returningItems: [item], completionHandler: nil)
+//        }
         #else
         let fileManager = FileManager.default
         let ruleFilePath = fileManager.containerURL(forSecurityApplicationGroupIdentifier: "group.nyamoblocker.blockerList")?.appendingPathComponent("blockerList.json")
@@ -52,7 +59,8 @@ class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
     }
     
     func beginRequest(with context: NSExtensionContext) {
-        
+        loadBlockerList(context: context)
+
         let enabled: Bool = UserDefaults(suiteName: "group.nyamoblocker.blockerList")?.object(forKey: "enabled") as? Bool ?? false
 
         switch enabled {
--- blockerList/genPatch.py
+++ blockerList/genPatch.py
@@ -3,17 +3,22 @@ import json
 blockerList = []
 with open("hosts.txt", mode="r") as f:
     for host in f:
-        if "127.0.0.1" in host:
-            domain = host.split(" ")[1].strip().replace(".", "\\.")
-            blockerList.append({"action": {"type": "block"}, "trigger" : {"url-filter": domain}})
-        if "::1" in host:
-            domain = host.split(" ")[1].strip().replace(".", "\\.")
+        if "//" not in host and host != "\n":
+            domain = host.strip().replace(".", "\\.")
             blockerList.append({"action": {"type": "block"}, "trigger" : {"url-filter": domain}})
 with open("selector.txt", mode="r") as f:
+    selectors = []
     for css in f:
-        if not "//" in css:
-            selector = css.split(" ")[0].strip()
-            domain = css.split(" ")[1].strip().replace(".", "\\.")
-            blockerList.append({"action": {"type": "css-display-none", "selector": selector}, "trigger" : {"url-filter": domain}})
+        if "//" in css: # Domain Name
+            domain = css.replace("//", "").strip().replace(".", "\\.")
+        else:
+            if css != "\n":
+                selectors.append(css.strip())
+            if css == "\n":
+                selector = ", ".join(selectors)
+                blockerList.append({"action": {"type": "css-display-none", "selector": selector}, "trigger" : {"url-filter": domain}})
+                selectors = []
+    selector = ", ".join(selectors)
+    blockerList.append({"action": {"type": "css-display-none", "selector": selector}, "trigger" : {"url-filter": domain}})
 with open("blockerList.json", mode="w") as w:
     w.write(json.dumps(blockerList, indent=4))
